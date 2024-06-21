89a16282c9308038fe3601f0bbfe7e0db75a9a6b
shaurma555 <denisenkokirill2011@gmail.com>
2024-06-21 19:00:15 +0300

    task9

---
+++ task9.py
@@ -0,0 +1,13 @@
+i=0
+#число делится на 5 и на 11
+for i in range(10,1000):
+    print(i)
+    if i == 148:
+        break
+        print(i)
+        
+        
+        
+    
+    
+              
